460bfcfa81689ff8a1308950f8e53c24444daac9
unknown <Administrator@SEC.CORE>
2021-03-15 14:06:16 +0100

    Småfiks

--- ScriptADOrganizationUnit.ps1
+++ ScriptADOrganizationUnit.ps1
@@ -1,20 +1,32 @@
+#Sender med fil som parameter
+param(
+    [parameter(mandatory=$true)]
+    [string] $file
+)
+
+#Sjekker om filen er en '.csv' fil
+if ( (Get-Item $file).Extension -eq ".csv"){
+
     #Filen "OUstructure.csv inneholder OU strukturen, og kan enkelt endres"
-Get-Content 'C:\Users\Admin\Desktop\albegra-a2\OUStructure.csv' | ForEach-Object{
-    $domainName = 'DC=sec,DC=core'
-    $ouPath = ''
-        #OU(Organizational Unit) og lager et reversjert array, slik at det leses fra toppen
-    $ou = (Split-Path $_ -Parent).Split('\')
-    [array]::Reverse($ou)
-    $ou | ForEach-Object{
-        if ($_.Length -eq 0){
-            return
+    Get-Content $file | ForEach-Object{
+        $domainName = 'DC=sec,DC=core'
+        $ouPath = ''
+            #OU(Organizational Unit) og lager et reversjert array, slik at det leses fra toppen
+        $ou = (Split-Path $_ -Parent).Split('\')
+        [array]::Reverse($ou)
+        $ou | ForEach-Object{
+            if ($_.Length -eq 0){
+                return
+            }
+            $ouPath = $ouPath + 'OU=' + $_ + ','
         }
-        $ouPath = $ouPath + 'OU=' + $_ + ','
+        $ouPath += $domainName
+    #echo $ouPath
+        $newOUName = split-path $_ -Leaf    
+    #echo $newOUName
+            #protect from accidential deletion kun i testing, fernes når produkt skal realiseres
+        New-ADOrganizationalUnit -Name $newOUName -Path $ouPath -ProtectedFromAccidentalDeletion $false
     }
-    $ouPath += $domainName
-#echo $ouPath
-    $newOUName = split-path $_ -Leaf    
-#echo $newOUName
-        #protect from accidential deletion kun i testing, fernes når produkt skal realiseres
-    New-ADOrganizationalUnit -Name $newOUName -Path $ouPath -ProtectedFromAccidentalDeletion $false
+} else {
+    Write-Output "Filen er ikke en '.csv' fil"
 }
