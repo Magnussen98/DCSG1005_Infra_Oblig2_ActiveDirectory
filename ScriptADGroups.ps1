6928020d43537443b8a16af3c13053b7da1d358a
Admin <Admin@sec.core>
2021-03-16 11:36:36 +0100

    Lagt til mulighet for å sende med fil, som scriptADOrganizoalUnit

--- ScriptADGroups.ps1
+++ ScriptADGroups.ps1
@@ -1,18 +1,27 @@
-Get-Content 'C:\Users\Admin\Desktop\albegra-a2\GroupStructure.csv' | ForEach-Object{
-$domainName = 'DC=sec,DC=core'
-$GroupPath = ''
-$Group = (Split-Path $_ -Parent).Split('\')
-[array]::Reverse($Group)
- $group | ForEach-Object{
-        if ($_.Length -eq 0){
-            return
+#Get-Content 'C:\Users\Admin\Desktop\albegra-a2\GroupStructure.csv' | ForEach-Object{
+#Sender med fil som parameter
+param(
+    [parameter(mandatory=$true)]
+    [string] $file
+)
+if ( (Get-Item $file).Extension -eq ".csv"){
+     #Filen "OUstructure.csv inneholder OU strukturen, og kan enkelt endres"
+    Get-Content $file | ForEach-Object{
+        $domainName = 'DC=sec,DC=core'
+        $GroupPath = ''
+        $Group = (Split-Path $_ -Parent).Split('\')
+        [array]::Reverse($Group)
+        $group | ForEach-Object{
+                if ($_.Length -eq 0){
+                    return
+                }
+                $GroupPath = $GroupPath + 'OU=' + $_ + ','
+            }
+            $GroupPath += $domainName
+            
+        #echo $GroupPath
+            $newGroupName = split-path $_ -Leaf  
+        #echo "G_$newGroupName"
+            New-ADGroup -GroupCategory Security -GroupScope Global -Name  "G_$newGroupName" -Path $GroupPath
         }
-        $GroupPath = $GroupPath + 'OU=' + $_ + ','
-    }
-    $GroupPath += $domainName
-    
-#echo $GroupPath
-    $newGroupName = split-path $_ -Leaf  
-#echo "G_$newGroupName"
-    New-ADGroup -GroupCategory Security -GroupScope Global -Name  "G_$newGroupName" -Path $GroupPath
 }
