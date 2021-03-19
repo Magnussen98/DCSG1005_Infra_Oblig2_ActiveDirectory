9c2ccec3ff3460b830d219437f1c1572188c7ee3
Admin <admin@SEC.CORE>
2021-03-19 13:59:07 +0100

    Kjørt script gjennom 'analyzer'

--- Groups/ScriptADGroups.ps1
+++ Groups/ScriptADGroups.ps1
@@ -18,14 +18,13 @@ if ( (Get-Item $file).Extension -eq ".csv"){
             }
             $GroupPath += $domainName
 
-            $newGroupName = split-path $_ -Leaf  
-        
+            $newGroupName = split-path $_ -Leaf
+
         if($GroupPath -like "OU=R_*"){
              New-ADGroup -GroupCategory Security -GroupScope DomainLocal -Name  "G_$newGroupName" -Path $GroupPath
         }
         else{
             New-ADGroup -GroupCategory Security -GroupScope Global -Name  "G_$newGroupName" -Path $GroupPath
         }
-          
-        }
+    }
 }
